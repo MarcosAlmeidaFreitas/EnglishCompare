4781641c5e5f3f2f2370f81a4abcba6c15acffe5
Erick Cesar <e.cesar88@gmail.com>
2025-02-23 13:10:59 -0300

    feat: add words

--- src/pages/Cadastrar/index.tsx
+++ src/pages/Cadastrar/index.tsx
@@ -1,13 +1,44 @@
-import { Button } from '../../Components/Button'
-import { Input } from '../../Components/Input'
-import { IoAddCircleSharp } from 'react-icons/io5';
+import { useState } from "react";
+import { Button } from "../../Components/Button";
+import { Input } from "../../Components/Input";
+import { IoAddCircleSharp } from "react-icons/io5";
 
 export function Home() {
+  const [words, setWords] = useState<Array<string>>([]);
+
+  const [newWord, setNewWord] = useState<string>("");
+
+  const handleAddNewWord = () => {
+    setWords((prev) => [...prev, newWord]);
+    setNewWord('')
+  };
+
   return (
-    <div className='flex gap-3'>
-      <h1 className="text-gray-50">teste</h1>
-      <Input placeholder='teste'></Input>
-      <Button iconRight={<IoAddCircleSharp className='text-gray-100 text-2xl' />}>teste</Button>
+    <div className="p-4 max-w-[1240px] mx-auto">
+      <div className="flex flex-col gap-3 ">
+        <div className="flex gap-3">
+          <Input
+            type="text"
+            placeholder="Digite uma nova palavra"
+            value={newWord}
+            onChange={(evt) => {
+              setNewWord(evt.target.value);
+            }}
+          />
+          <Button
+            onClick={handleAddNewWord}
+            iconLeft={<IoAddCircleSharp className="text-gray-100 text-2xl" />}
+          >
+            Adicionar
+          </Button>
+        </div>
+      </div>
+
+      <div className="pt-5 text-gray-100">
+        {words.map((word) => (
+          <p key={word}>{word}</p>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
